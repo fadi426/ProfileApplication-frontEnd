d744698dff26ed6189270fdf3eb791d706b803b2
fadi426 <fadiqadadi@hotmail.com>
2019-06-23 18:17:16 +0200

    Weather page css changes
    -Reconstructed laybout
    -Added Iframe

--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
-
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +8,17 @@ import { Router } from "@angular/router";
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  location = '';
 
   constructor(private router: Router) {}
 
   routeTo(component){
     this.router.navigate([component]);
   }
+  
+    onKey(event: any) { // without type info
+      this.location += event.target.value + ' | ';
+    }
 
   ngOnInit() {
   }
