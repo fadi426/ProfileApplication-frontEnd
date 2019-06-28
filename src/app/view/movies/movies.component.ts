bc16dcc18bff46eadd8300cd46e9fc0c3ada8c4c
fadi426 <fadiqadadi@hotmail.com>
2019-06-28 15:56:23 +0200

    Finished Movies
    -Added html/css/js code for the movies component.
    -Added loaders for news/movies/weather/events
    -Bugfixes on navbar and weather component responsiveness

--- src/app/view/movies/movies.component.ts
+++ src/app/view/movies/movies.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { StoreModule } from '../../modules/store/store.module';
 
 @Component({
   selector: 'app-movies',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent implements OnInit {
+  constructor(private store : StoreModule) { }
 
-  constructor() { }
+  movies = [];
+  
+  async getMovies() {
+    const resp = await fetch("https://localhost:5001/api/profile/locations/" + this.store.location._country + "/movies")
+    const data = await resp.json();
+    this.movies = data;
+  }
+
+  openMovieLink(url){
+    window.open(url, '_blank');
+  }
 
   ngOnInit() {
+    this.getMovies();
   }
 
 }
